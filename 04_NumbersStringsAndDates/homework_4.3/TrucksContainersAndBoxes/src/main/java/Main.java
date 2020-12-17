63be9c72cb4e2c967198a501fb10e7d4b472d444
AlexSOLO <dvjsalex@gmail.com>
2020-12-17 16:00:38 +0500

    4.17 homework 4.3*

--- 04_NumbersStringsAndDates/homework_4.3/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.3/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -5,20 +5,31 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String boxes = scanner.nextLine();
+        int input = Integer.parseInt(boxes);
+        int container = 1;
+        int truck = 1;
+        int fullnessOfContainer = 27;
+        int fullnessOfTruck = 12;
 
-        // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
-        // пример вывода при вводе 2
-        // для отступа используйте табуляцию - \t
-
-        /*
-        Грузовик: 1
-            Контейнер: 1
-                Ящик: 1
-                Ящик: 2
-        Необходимо:
-        грузовиков - 1 шт.
-        контейнеров - 1 шт.
-        */
+        if (input > 0) {
+            System.out.println("Грузовик: " + truck);
+            System.out.println("\tКонтейнер: " + container);
+            for (int i = 1; i <= input; i++) {
+                System.out.println("\t\tЯщик: " + i);
+                if (i % (fullnessOfTruck * fullnessOfContainer) == 0 && i != input) {
+                    truck++;
+                    System.out.println("Грузовик: " + truck);
+                }
+                if (i % fullnessOfContainer == 0 && i != input) {
+                    container++;
+                    System.out.println("\tКонтейнер: " + container);
+                }
+            }
+            System.out.println("Необходимо:" + System.lineSeparator() + "грузовиков - " + truck + " шт."
+                    + System.lineSeparator() + "контейнеров - " + container + " шт.");
+        } else {
+            System.out.println("Необходимо:" + System.lineSeparator() + "грузовиков - 0 шт."
+                    + System.lineSeparator() + "контейнеров - 0 шт.");
+        }
     }
-
 }
