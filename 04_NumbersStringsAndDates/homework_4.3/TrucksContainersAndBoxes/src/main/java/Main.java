dcf94d8f35cd909d6ee63bbe8d061181dd5e9259
AlexSOLO <dvjsalex@gmail.com>
2020-12-18 08:50:06 +0500

    repair 4.17 homework 4.3*

--- 04_NumbersStringsAndDates/homework_4.3/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.3/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class Main {
+    final static int BOXES_IN_CONTAINER = 27;
+    final static int CONTAINERS_IN_TRUCK = 12;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -8,19 +10,18 @@ public class Main {
         int input = Integer.parseInt(boxes);
         int container = 1;
         int truck = 1;
-        int fullnessOfContainer = 27;
-        int fullnessOfTruck = 12;
+        int capacityTruck = CONTAINERS_IN_TRUCK * BOXES_IN_CONTAINER;
 
         if (input > 0) {
             System.out.println("Грузовик: " + truck);
             System.out.println("\tКонтейнер: " + container);
             for (int i = 1; i <= input; i++) {
                 System.out.println("\t\tЯщик: " + i);
-                if (i % (fullnessOfTruck * fullnessOfContainer) == 0 && i != input) {
+                if (i % capacityTruck == 0 && i != input) {
                     truck++;
                     System.out.println("Грузовик: " + truck);
                 }
-                if (i % fullnessOfContainer == 0 && i != input) {
+                if (i % BOXES_IN_CONTAINER == 0 && i != input) {
                     container++;
                     System.out.println("\tКонтейнер: " + container);
                 }
