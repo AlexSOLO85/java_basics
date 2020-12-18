4dfe3399970394c7d4da5d8b68ac08f5ed60337e
AlexSOLO <dvjsalex@gmail.com>
2020-12-18 11:29:05 +0500

    4.21 homework 4.4 task 2

--- 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
@@ -1,9 +1,12 @@
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
-  }
-
+        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+        int num1 = Integer.parseInt(text.substring(15, 19));
+        int num2 = Integer.parseInt(text.substring(35, 39));
+        int num3 = Integer.parseInt(text.substring(56, 61));
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+    }
 }
