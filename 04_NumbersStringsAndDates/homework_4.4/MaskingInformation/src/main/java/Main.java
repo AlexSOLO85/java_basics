229fc4bf0a320f19180e0d481f2286d22a3ccad8
AlexSOLO <dvjsalex@gmail.com>
2020-12-23 16:40:44 +0500

    repair 4.21 homework 4.4 task 3*

--- 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
@@ -1,27 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        String safe = searchAndReplaceDiamonds("Номер кредитной карты <4008> 1234 <5678> 8912", "***");
+        String safe = searchAndReplaceDiamonds("Номер кредитной карты <4008 1234 5678> 8912", "***");
         System.out.println(safe);
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
-        String beginText;
-        String endText;
-        int beginIndex;
-        int endIndex;
-        String fullString = text;
-        boolean isDiamonds = fullString.contains("<") && fullString.contains(">");
-
-        beginIndex = fullString.indexOf('<');
-        endIndex = fullString.indexOf('>') + 1;
-
-        if (isDiamonds) {
-            beginText = text.substring(0, beginIndex);
-            endText = text.substring(endIndex);
-            fullString = beginText + placeholder + endText;
-            return fullString;
-        }
-        return text;
+        return text.replaceAll("<\\w.*?>", placeholder);
     }
 }
