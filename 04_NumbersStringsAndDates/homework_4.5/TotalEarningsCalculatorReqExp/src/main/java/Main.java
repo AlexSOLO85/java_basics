ab19ba5d1e1e3fb82e6580d2a309e91224aeaad6
AlexSOLO <dvjsalex@gmail.com>
2020-12-23 23:14:59 +0500

    4.25 homework 4.5 task 1

--- 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -1,12 +1,24 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        calculateSalarySum("Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей");
+    }
 
-  }
-
-  public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
-  }
-
+    public static int calculateSalarySum(String text) {
+        int countSum = 0;
+        Pattern p = Pattern.compile("\\d+");
+        Matcher m = p.matcher(text);
+        while (m.find()) {
+            countSum += Integer.parseInt(m.group());
+        }
+        if (countSum > 0) {
+            System.out.println("Общая сумма заработка: " + countSum);
+        } else {
+            System.out.println("Никто ничего не заработал");
+        }
+        return countSum;
+    }
 }
