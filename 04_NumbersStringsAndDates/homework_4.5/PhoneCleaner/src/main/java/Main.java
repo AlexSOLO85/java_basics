530225236b695bee54785539c58fc89252535055
AlexSOLO <dvjsalex@gmail.com>
2020-12-26 14:33:58 +0500

    4.25 homework 4.5 task 4

--- 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
@@ -1,17 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
+    private static final String REG_VALUE = "^((8|\\+7|7)[\\-\\s])?(\\(?\\d{3}\\)?[\\-\\s])([\\d\\s\\-]{7,9})$";
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+            String phoneNumber = input.replaceAll("[^0-9]", "").
+                    trim().replaceAll("^(8)", "7").trim();
+            if (phoneNumber.length() == 10) {
+                phoneNumber = "7" + phoneNumber;
+            }
+            if (input.matches(REG_VALUE)) {
+                System.out.println(phoneNumber);
+            } else {
+                System.out.println("Неверный формат номера");
+            }
+        }
     }
-  }
-
 }
