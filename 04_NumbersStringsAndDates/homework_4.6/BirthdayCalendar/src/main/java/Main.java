927db1a0b289ace5622f30f69907b16972cfd23c
AlexSOLO <dvjsalex@gmail.com>
2020-12-28 20:24:53 +0500

    repair 4.27 homework 4.6

--- 04_NumbersStringsAndDates/homework_4.6/BirthdayCalendar/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.6/BirthdayCalendar/src/main/java/Main.java
@@ -4,17 +4,18 @@ import java.util.GregorianCalendar;
 import java.util.Locale;
 
 public class Main {
-    private static int DAY = 31;
-    private static int MONTH = 12;
-    private static int YEAR = 1990;
+    private static int DAY_OF_BIRTH = 31;
+    private static int MONTH_OF_BIRTH = 12;
+    private static int YEAR_OF_BIRTH = 1990;
+    private static String FORMAT_DATE = " - dd.MM.yyyy - EE";
 
     public static void main(String[] args) {
-        System.out.println(collectBirthdays(YEAR, MONTH, DAY));
+        System.out.println(collectBirthdays(YEAR_OF_BIRTH, MONTH_OF_BIRTH, DAY_OF_BIRTH));
     }
 
     public static String collectBirthdays(int year, int month, int day) {
         StringBuilder builder = new StringBuilder();
-        SimpleDateFormat dateFormat = new SimpleDateFormat(" - dd.MM.yyyy - EE", Locale.ENGLISH);
+        SimpleDateFormat dateFormat = new SimpleDateFormat(FORMAT_DATE, Locale.ENGLISH);
         Calendar birthDate = new GregorianCalendar(year, month - 1, day, 0, 0, 0);
         Calendar currentDate = Calendar.getInstance();
         int number = 0;
