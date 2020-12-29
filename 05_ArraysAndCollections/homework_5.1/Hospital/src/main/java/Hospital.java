8468c76c00c4a5800067dc2d6be7b19b3f6526b4
AlexSOLO <dvjsalex@gmail.com>
2020-12-29 15:10:09 +0500

    5.5 homework 5.1 task 2

--- 05_ArraysAndCollections/homework_5.1/Hospital/src/main/java/Hospital.java
+++ 05_ArraysAndCollections/homework_5.1/Hospital/src/main/java/Hospital.java
@@ -1,23 +1,45 @@
+import java.util.Arrays;
+
 public class Hospital {
+    private static float MIN_TEMP = 32.0f;
+    private static float MAX_TEMP = 40.0f;
 
     public static float[] generatePatientsTemperatures(int patientsCount) {
-
-        //TODO: напишите метод генерации массива температур пациентов
-
-        return new float[0];
+        float[] temperatureData = new float[patientsCount];
+        for (int i = 0; i < temperatureData.length; i++) {
+            temperatureData[i] = generateRandomTemperature(MIN_TEMP, MAX_TEMP);
+        }
+        return temperatureData;
     }
 
     public static String getReport(float[] temperatureData) {
-        /*
-        TODO: Напишите код, который выводит среднюю температуру по больнице,количество здоровых пациентов,
-            а также температуры всех пациентов.
-        */
+        float sumTemperatures = 0f;
+        int healthyPatients = 0;
+        float[] patientsTemperatures = generatePatientsTemperatures(temperatureData.length);
+
+        for (float temperature : patientsTemperatures) {
+            sumTemperatures += temperature;
+            if (temperature >= 36.2f && temperature <= 36.9f) {
+                healthyPatients++;
+            }
+        }
 
         String report =
-                "Температуры пациентов: " + 0 +
-                        "\nСредняя температура: " + 0 +
-                        "\nКоличество здоровых: " + 0;
-
+                "Температуры пациентов: " + printArray(patientsTemperatures) +
+                        "\nСредняя температура: " + getAverageTemperatures(sumTemperatures, patientsTemperatures) +
+                        "\nКоличество здоровых: " + healthyPatients;
         return report;
+    }
+
+    private static float generateRandomTemperature(float min, float max) {
+        return Math.round(((Math.random() * (max - min) + min)) * 10f) / 10f;
+    }
+
+    private static float getAverageTemperatures(float sumTemp, float[] patientsTemp) {
+        return (float) Math.round((sumTemp / patientsTemp.length) * 100f) / 100f;
+    }
+
+    private static String printArray(float[] array) {
+        return Arrays.toString(array).replaceAll("[\\[\\],]", "");
     }
 }
