c0e6e9693754b056a6caec4e137cb928bafb6d3d
AlexSOLO <dvjsalex@gmail.com>
2021-02-11 11:40:34 +0500

    7.8 homework 7.2 task 1

--- 07_AdvancedOOPFeatures/homework_7.2/Employees/src/main/java/Main.java
+++ 07_AdvancedOOPFeatures/homework_7.2/Employees/src/main/java/Main.java
@@ -1,18 +1,24 @@
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class Main {
 
     private static final String STAFF_TXT = "data/staff.txt";
+    private static final int YEAR = 2017;
 
     public static void main(String[] args) {
         List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
-        Employee employeeMaxSalary = findEmployeeWithHighestSalary(staff, 2017);
+        Employee employeeMaxSalary = findEmployeeWithHighestSalary(staff, YEAR);
         System.out.println(employeeMaxSalary);
     }
 
     public static Employee findEmployeeWithHighestSalary(List<Employee> staff, int year) {
-        //TODO Метод должен вернуть сотрудника с максимальной зарплатой среди тех,
-        // кто пришёл в году, указанном в переменной year
-        return null;
+        Optional<Employee> optional = staff.stream().
+                filter(e -> LocalDate.ofInstant(e.getWorkStart().toInstant(), ZoneId.systemDefault()).getYear() == year).
+                max(Comparator.comparing(Employee::getSalary));
+        return optional.orElseGet(optional::get);
     }
 }
