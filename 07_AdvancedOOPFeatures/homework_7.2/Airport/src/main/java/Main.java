add7c476d1be60a687a726f192e4f6475f109649
AlexSOLO <dvjsalex@gmail.com>
2021-02-12 08:08:42 +0500

    7.8 homework 7.2 task 2

--- 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
+++ 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
@@ -1,18 +1,35 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
 
-import java.util.Collections;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
+    private static final int HOUR = 2;
+
     public static void main(String[] args) {
-
-
+        Airport airport = Airport.getInstance();
+        findPlanesLeavingInTheNextTwoHours(airport);
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+        LocalDateTime localDateTimeOne = LocalDateTime.now();
+        LocalDateTime localDateTimeTwo = localDateTimeOne.plusHours(HOUR);
+        List<Flight> flightList = airport.getTerminals().stream()
+                .flatMap(terminal -> terminal.getFlights().stream())
+                .filter(s -> convertToLocalDateTime(s.getDate()).getHour() >= localDateTimeOne.getHour() &
+                        convertToLocalDateTime(s.getDate()).getHour() <= localDateTimeTwo.getHour() &
+                        s.getType().equals(Flight.Type.DEPARTURE))
+                .collect(Collectors.toList());
+        flightList.forEach(System.out::println);
+        return flightList;
     }
 
+    private static LocalDateTime convertToLocalDateTime(Date dateToConvert) {
+        return LocalDateTime.ofInstant(
+                dateToConvert.toInstant(), ZoneId.systemDefault());
+    }
 }
