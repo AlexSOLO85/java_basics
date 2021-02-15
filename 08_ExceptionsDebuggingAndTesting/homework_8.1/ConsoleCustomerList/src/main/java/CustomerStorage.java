8d64a7a28edc092544848393046674b4d91899fb
AlexSOLO <dvjsalex@gmail.com>
2021-02-15 13:13:22 +0500

    8.4 homework 8.1

--- 08_ExceptionsDebuggingAndTesting/homework_8.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ 08_ExceptionsDebuggingAndTesting/homework_8.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -2,6 +2,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CustomerStorage {
+    private static final String REGEX_PHONE = "^(\\+7)\\d{10}$";
+    private static final String REGEX_EMAIL = ".+@.+\\..+";
+    private static final int LENGTH_INPUT = 4;
     private final Map<String, Customer> storage;
 
     public CustomerStorage() {
@@ -15,6 +18,15 @@ public class CustomerStorage {
         final int INDEX_PHONE = 3;
 
         String[] components = data.split("\\s+");
+        if (components.length != LENGTH_INPUT) {
+            throw new IndexOutOfBoundsException("Неверный формат ввода! Пример ввода:\n" + Main.getAddCommand());
+        }
+        if (!components[INDEX_PHONE].matches(REGEX_PHONE)) {
+            throw new IllegalArgumentException("Неверный формат номера!");
+        }
+        if (!components[INDEX_EMAIL].matches(REGEX_EMAIL)) {
+            throw new IllegalArgumentException("Неверный формат email!");
+        }
         String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
         storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
     }
