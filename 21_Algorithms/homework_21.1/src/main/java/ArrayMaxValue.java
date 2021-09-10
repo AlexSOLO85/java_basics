37a0f4a8ebe37aa91cb292ba0e3d388ec31366c1
AlexSOLO <dvjsalex@gmail.com>
2021-09-10 10:52:30 +0500

    add Algorithms

---
+++ 21_Algorithms/homework_21.1/src/main/java/ArrayMaxValue.java
@@ -0,0 +1,13 @@
+package array_max_value;
+
+public class ArrayMaxValue {
+    public static int getMaxValue(int[] values) {
+        int maxValue = Integer.MIN_VALUE;
+        for (int value : values) {
+            if (value > maxValue) {
+                maxValue = value;
+            }
+        }
+        return maxValue;
+    }
+}
